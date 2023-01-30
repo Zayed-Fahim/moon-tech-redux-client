e0d8e3efe4d8db6351cc1ef5eee197b74eef7502
Zayed Fahim <zayedfahim33.cse@gmail.com>
2023-01-30 19:51:15 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main/Main";
+import About from "../../Pages/About/About";
+import Cart from "../../Pages/Cart/Cart";
+import Home from "../../Pages/Home/Home";
+import TopRated from "../../Pages/TopRated/TopRated";
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "top-rated",
+        element: <TopRated />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+export default routes;
